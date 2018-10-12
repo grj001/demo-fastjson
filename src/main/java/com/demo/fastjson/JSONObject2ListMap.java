988d5b55b1177e52cda66e33649f8ad064e4116f
grj001 <875654381@qq.com>
2018-10-12 09:14:10 +0800

    1

--- src/main/java/com/demo/fastjson/JSONObject2ListMap.java
+++ src/main/java/com/demo/fastjson/JSONObject2ListMap.java
@@ -2,12 +2,19 @@ package com.demo.fastjson;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class JSONObject2ListMap {
 
-    public void jSONObject2ListMap(){
+    /**
+     *
+     */
+    public void jSONArrayObjectStr2ListMap(){
         String jsonStr = "[" +
                 "{'fac_name':1,'fac_address':1}" +
                 ",{'fac_name':2,'fac_address':2}" +
@@ -15,19 +22,33 @@ public class JSONObject2ListMap {
                 ",{'fac_name':4,'fac_address':4}" +
                 ",{'fac_name':5,'fac_address':5}]";
 
+        ArrayList<HashMap<String,Object>> lm = new ArrayList<HashMap<String, Object>>();
+        HashMap<String,Object> m = new HashMap<String,Object>();
+
         JSONArray jsonArray = JSON.parseArray(jsonStr);
         jsonArray.forEach(x ->{
+            JSONObject xo = (JSONObject) x;
+            Set<Map.Entry<String, Object>> entries = xo.entrySet();
+            //Set<Map.Entry<String, Object>>
+            entries.forEach(y -> {
+                //Map.Entry<String, Object>
+                m.put(y.getKey(),y.getValue());
+            });
+            System.out.printf("一个map m : %s\n",m);
 
-            System.out.printf("x : %s\n",x);
-
+            //需要新建一个map 地址 用于存放数据
+            HashMap<String,Object> m2 = new HashMap<String,Object>();
+            m2.putAll(m);
+            lm.add(m2);
+            m.clear();
         });
-
+        System.out.printf("结果为 lm : %s\n",lm);
     }
 
 
     public static void main(String[] args){
         JSONObject2ListMap jsonObject2ListMap = new JSONObject2ListMap();
-        jsonObject2ListMap.jSONObject2ListMap();
+        jsonObject2ListMap.jSONArrayObjectStr2ListMap();
     }
 
 }
